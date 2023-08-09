0feb74417f04943290449758266dbbaec6754e29
bales <ssaasshh29@gmail.com>
2023-08-09 15:13:54 +0300

    9/8 update 2

--- react/exsersizes listed/exsursizeusermanagmentapp/src/pages/UsersPage.js
+++ react/exsersizes listed/exsursizeusermanagmentapp/src/pages/UsersPage.js
@@ -1,20 +1,49 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function UsersPage() {
   const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     let userData = JSON.parse(localStorage.getItem("userData"));
     setUsers(userData);
   }, []);
 
-  console.log("are you working , yes");
+  const handleDelete = (i) => {
+    if (!window.confirm(`Are you sure you want to delete`)) {
+      return;
+    }
+
+    const updateData = [...users];
+    updateData.splice(i, 1);
+    setUsers(updateData);
+
+    localStorage.setItem("userData", JSON.stringify(updateData));
+  };
+
   return (
     <div className="container">
+      <button
+        className="btn btn-primary mb-3"
+        onClick={() => navigate("/add-user")}
+      >
+        Add User
+      </button>
       <ul className="list-group">
-        {users.map((user) => (
+        {users.map((user, i) => (
           <li key={user.id} className="list-group-item">
             {user.firstName} {user.lastName} - {user.email}
+            <button
+              className="btn btn-info"
+              onClick={() => navigate(`/edit-user/${user.id}`)}
+            >
+              Edit
+            </button>
+            <button
+              className="btn btn-warning"
+              onClick={() => handleDelete(i)}
+            ></button>
           </li>
         ))}
       </ul>
