9aa0256dde9d2a0c518b0ebaaef668eeb4185358
BaleusBlueberry <ssaasshh29@gmail.com>
2023-08-31 07:30:40 +0300

    created createcomp component

--- react/final project/finalprojectv2/src/Pages/RegisterPage.js
+++ react/final project/finalprojectv2/src/Pages/RegisterPage.js
@@ -1,18 +1,54 @@
 import { useState } from "react";
 import Container from "react-bootstrap/Container";
+import Dropdown from "react-bootstrap/Dropdown";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
+import CreateComp from "../Resources/CreateComp";
 
 function RegisterPage() {
   const [registerData, setRegisterData] = useState({
+    id: Math.floor(Math.random() * 10000000000000),
+    firstName: "",
+    lastName: "",
     email: "",
     password: "",
+    isBusines: false,
   });
 
   return (
     <Container>
+      <FloatingLabel controlId="firstName" label="firstName">
+        <Form.Control
+          type="text"
+          placeholder="Password"
+          name="firstName"
+          value={registerData.firstName}
+          onChange={(e) =>
+            setRegisterData({ ...registerData, firstName: e.target.value })
+          }
+        />
+      </FloatingLabel>
+      <FloatingLabel controlId="lastName" label="lastName">
+        <Form.Control
+          type="text"
+          placeholder="lastName"
+          name="lastName"
+          value={registerData.lastName}
+          onChange={(e) =>
+            setRegisterData({ ...registerData, lastName: e.target.value })
+          }
+        />
+      </FloatingLabel>
       <FloatingLabel controlId="email" label="Email address" className="mb-3">
-        <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Control
+          type="email"
+          placeholder="name@example.com"
+          name="email"
+          value={registerData.email}
+          onChange={(e) =>
+            setRegisterData({ ...registerData, email: e.target.value })
+          }
+        />
       </FloatingLabel>
       <FloatingLabel controlId="Password" label="Password">
         <Form.Control
@@ -25,6 +61,29 @@ function RegisterPage() {
           }
         />
       </FloatingLabel>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="isBusnies">
+          Is it a Business?
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item
+            onClick={() =>
+              setRegisterData({ ...registerData, isBusines: false })
+            }
+          >
+            Not a Busines
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() =>
+              setRegisterData({ ...registerData, isBusines: true })
+            }
+          >
+            Is a Busines
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <CreateComp name="testname" type="test type" />
     </Container>
   );
 }
---
+++ react/final project/finalprojectv2/src/Resources/CreateComp.js
@@ -0,0 +1,10 @@
+function CreateComp(props) {
+  return (
+    <>
+      <h1>{props.type}</h1>
+      <h1>{props.name}</h1>
+    </>
+  );
+}
+
+export default CreateComp;
