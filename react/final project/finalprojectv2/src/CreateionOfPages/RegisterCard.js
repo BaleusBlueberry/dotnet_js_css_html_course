b520c18d1f65ea4408ab2e3deaf181db6705db06
BaleusBlueberry <ssaasshh29@gmail.com>
2023-09-11 07:18:10 +0300

    ./

--- react/final project/finalprojectv2/src/CreateionOfPages/RegisterCard.js
+++ react/final project/finalprojectv2/src/CreateionOfPages/RegisterCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import CreateComp from "../ResourcesProject/CreateComp";
 import Container from "react-bootstrap/Container";
 import axios from "axios";
